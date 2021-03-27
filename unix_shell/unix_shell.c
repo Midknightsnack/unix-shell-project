1acd9208b47a447a9d0676ce6ce935a1ab97b470
Bradley Diep <42824199+Midknightsnack@users.noreply.github.com>
2021-03-26 17:32:22 -0700

    Add files via upload

--- unix_shell/unix_shell.c
+++ unix_shell/unix_shell.c
@@ -10,7 +10,6 @@
 #include <sys/wait.h>
 #include <string.h>
 
-#define MAX_LINE 80     //maximum length command
 
 int find_pipe(char** cargs){
     int index = 0;
@@ -24,33 +23,33 @@ int find_pipe(char** cargs){
 }
 
 
-char** token_key(char* input, char* cargs[]){
-    const char delimeter[2] = {' ', '\t'}; // delimiters (space and tab)
+char** parse(char* input, char* cargs[]){
+    const char break_chars[2] = {' ', '\t'}; // delimiters to split string (space and tab)
     char* token;
     int num_args = 0;
 
-    token = strtok(input, delimeter);
+    token = strtok(input, break_chars);
     cargs[0] = token;
 
     char** redirect = malloc(2 * sizeof(char*));
     for(int i = 0; i < 2; i++){
-        redirect[i] = malloc(MAX_LINE * sizeof(char));
+        redirect[i] = malloc(BUFSIZ * sizeof(char));
     }
     redirect[0] = ""; // input/output/pipe
     redirect[1] = ""; // path
 
     while(token != NULL){
         // add token to cargs
-        token = strtok(NULL, delimeter);
+        token = strtok(NULL, break_chars);
         if(token == NULL) break;
         if(!strncmp(token, ">", 1)){
-            token = strtok(NULL, delimeter);
+            token = strtok(NULL, break_chars);
             redirect[0] = "o";     //output
             redirect[1] = token;
             return redirect;
         } 
         else if(!strncmp(token, "<", 1)){
-            token = strtok(NULL, delimeter);
+            token = strtok(NULL, break_chars);
             redirect[0] = "i";     //input
             redirect[1] = token;
             return redirect;
@@ -65,23 +64,23 @@ char** token_key(char* input, char* cargs[]){
 
 
 int main(int argc, const char* argv[]){
-    char input[MAX_LINE];
-    char recent[MAX_LINE]; 
-    int pipefd[2];         // file descriptor
+    char input[BUFSIZ];
+    char recent[BUFSIZ]; // recent command
+    int fd[2];           // file descriptor
     int running = 1;
 
     // clear buffers
-    memset(input, 0, MAX_LINE * sizeof(char));
-    memset(recent, 0, MAX_LINE * sizeof(char));
+    memset(input, 0, BUFSIZ * sizeof(char));
+    memset(recent, 0, BUFSIZ * sizeof(char));
 
     while(running){
         printf("osh> ");  // command line prefix
         fflush(stdout);   // force flush to console
         
-        fgets(input, MAX_LINE, stdin);   // get inputs
-        input[strlen(input) - 1] = '\0'; // replace newline with null
+        fgets(input, BUFSIZ, stdin);   // get inputs
+        input[strlen(input) - 1] = '\0'; // wipe out newline at end of string
     
-        if(strncmp(input, "exit()", 6) == 0) //leave program when "osh> exit()"
+        if(strncmp(input, "exit", 4) == 0) //leave program when "osh> exit" 
             return 0;
         if(strncmp(input, "!!", 2)) // repeat recently used command
             strcpy(recent, input);      
@@ -106,12 +105,12 @@ int main(int argc, const char* argv[]){
             }
         }
         else{                         // child process
-            char* cargs[MAX_LINE];    // buffer to hold command line arguments
-            memset(cargs, 0, MAX_LINE * sizeof(char));
+            char* cargs[BUFSIZ];      // buffer to hold command line arguments
+            memset(cargs, 0, BUFSIZ * sizeof(char));
 
             int history = 0;
             if(!strncmp(input, "!!", 2)) history = 1;
-            char** redirect = token_key((history ? recent : input), cargs);
+            char** redirect = parse((history ? recent : input), cargs);
  
             if(history && recent[0] == '\0'){
                 printf("No commands in history.\n"); 
@@ -125,32 +124,32 @@ int main(int argc, const char* argv[]){
             }else if(!strncmp(redirect[0], "i", 1)){ // redirect input
                 printf("Reading from file: ./%s\n", redirect[1]);
                 int folder = open(redirect[1], O_RDONLY); // read-only
-                memset(input, 0, MAX_LINE * sizeof(char));
-                read(folder, input,  MAX_LINE * sizeof(char));
-                memset(cargs, 0, MAX_LINE * sizeof(char));
+                memset(input, 0, BUFSIZ * sizeof(char));
+                read(folder, input,  BUFSIZ * sizeof(char));
+                memset(cargs, 0, BUFSIZ * sizeof(char));
                 input[strlen(input) - 1]  = '\0';
-                token_key(input, cargs);
+                parse(input, cargs);
             }else if(!strncmp(redirect[0], "p", 1)){ // pipe found
                 pid_t pidc; // hierachical child
                 int right_pipe_offset = find_pipe(cargs);
                 cargs[right_pipe_offset] = "\0";
 
-                int fail_pipe = pipe(pipefd);
+                int fail_pipe = pipe(fd);
                 if(fail_pipe < 0){
                     fprintf(stderr, "Pipe creation failed...\n");
                     return 1;
                 }
 
-                char* left[MAX_LINE];
-                char* right[MAX_LINE];
-                memset(left, 0, MAX_LINE*sizeof(char));
-                memset(right, 0, MAX_LINE*sizeof(char));
+                char* left[BUFSIZ];
+                char* right[BUFSIZ];
+                memset(left, 0, BUFSIZ*sizeof(char));
+                memset(right, 0, BUFSIZ*sizeof(char));
 
                 for(int i = 0; i < right_pipe_offset; ++i){
                     left[i] = cargs[i];
                 }
 
-                for(int i = 0; i < MAX_LINE; ++i){
+                for(int i = 0; i < BUFSIZ; ++i){
                     int index = i + right_pipe_offset + 1;
                     if(cargs[index] == 0) break;
                     right[i] = cargs[index];
@@ -162,13 +161,13 @@ int main(int argc, const char* argv[]){
                     return 1;
                 }
                 if(pidc != 0){ //parent process 
-                    dup2(pipefd[1], STDOUT_FILENO); //duplicate stdout to write end of file descriptor
-                    close(pipefd[1]); //close write end of pipe
+                    dup2(fd[1], STDOUT_FILENO); //duplicate stdout to write end of file descriptor
+                    close(fd[1]); //close write end of pipe
                     execvp(left[0], left); //execute command in child
                     exit(0); 
                 }else{ //child process
-                    dup2(pipefd[0], STDIN_FILENO); //copy read end of pipe to stdin
-                    close(pipefd[0]); //close read end of pipe
+                    dup2(fd[0], STDIN_FILENO); //copy read end of pipe to stdin
+                    close(fd[0]); //close read end of pipe
                     execvp(right[0], right); //execute command in child
                     exit(0); 
                 }
